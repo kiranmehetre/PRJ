3357f5c616eaba78adf48f035f21db9b5ccf97a8
Suraj Chandgude <surajchandgude0304@gmail.com>
2020-03-26 13:47:24 +0530

    Added build conf

--- src/app/config/constants.ts
+++ src/app/config/constants.ts
@@ -2,6 +2,6 @@ export const AppConst = {
 
 
     // API_END_POINT : 'http://142.93.250.61:3000/',
-    API_END_POINT : "http://142.93.250.61:3000/",
+    API_END_POINT : "https://bit.ly/33MMIgN",
     pageLength: 25
 };
